75853e574dd8dbf74924886c591745254078f99e
Griff Hall <griffhall1@gmail.com>
2014-11-25 16:58:47 -0600

    value is stored persistently

--- persistent_storage/ViewController.swift
+++ persistent_storage/ViewController.swift
@@ -11,8 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        /* basically, you run the NSU.. lines once, then it's stored
+            make sure to add the exclamation point for the optional
+        */
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            // Do any additional setup after loading the view, typically from a nib.
+        
+        // NSUserDefaults.standardUserDefaults().setObject("Rob", forKey: "myName")
+        
+        // NSUserDefaults.standardUserDefaults().synchronize()
+        
+            // essentially just saves above value permanently ^^
+        
+        println(NSUserDefaults.standardUserDefaults().objectForKey("myName")!)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
